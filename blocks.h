745007b6fc80e75485cd1b04ba0651e95d4afd82
arthur <arthurfleck8@proton.me>
2024-07-04 11:27:22 +0200

    my config

---
+++ blocks.h
@@ -0,0 +1,17 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	// {"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 30,	0},
+
+	{"", "i3weather dwm", 300, 0},
+	{"", "~/my/scripts/dwm-volume.sh", 10, 0},
+	{"", "~/my/scripts/dwm-dunst.sh", 10, 0},
+	{"", "~/my/scripts/dwm-licht.sh",10, 0},
+	{"", "dwm_disk_space", 30, 0},
+	{"", "i3vpn dwm", 10, 0},
+	{"", "~/my/scripts/dwm-date.sh", 1,	0},
+};
+
+//sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
