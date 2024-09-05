5b7db6a3b86097fe36bfa363ca869befa4b65641
arthur <arthurfleck8@proton.me>
2024-09-05 14:27:42 +0200

    remove my prefix

--- blocks.h
+++ blocks.h
@@ -3,13 +3,13 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	// {"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 30,	0},
 
-	{"", "~/my/scripts/bb/weather.clj dwm", 300, 0},
-	{"", "~/my/scripts/dwm-volume.sh", 10, 0},
-	{"", "~/my/scripts/dwm-dunst.sh", 10, 0},
-	{"", "~/my/scripts/dwm-licht.sh",10, 0},
-	{"", "~/my/scripts/bb/dwm_disk_space.clj", 30, 0},
-	{"", "~/my/scripts/bb/i3vpn.clj dwm", 10, 0},
-	{"", "~/my/scripts/dwm-date.sh", 1,	0},
+	{"", "~/scripts/bb/weather.clj dwm", 300, 0},
+	{"", "~/scripts/dwm-volume.sh", 10, 0},
+	{"", "~/scripts/dwm-dunst.sh", 10, 0},
+	{"", "~/scripts/dwm-licht.sh",10, 0},
+	{"", "~/scripts/bb/dwm_disk_space.clj", 30, 0},
+	{"", "~/scripts/bb/i3vpn.clj dwm", 10, 0},
+	{"", "~/scripts/dwm-date.sh", 1,	0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
