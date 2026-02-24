6865438c5210553e1e994b8e64e29387f59a9f0d
arthur <arthurfleck8@proton.me>
2026-02-24 17:01:22 +0100

    current config

--- blocks.h
+++ blocks.h
@@ -1,11 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "~/scripts/bb/weather.clj dwm", 360,	0},
-	{"", "~/scripts/bb/bsd.clj", 10,	0},
 	{"", "~/backlight.sh", 30,	0},
-	{"", "~/batinfo.sh", 30,	0},
-	{"", "~/scripts/legacy-xorg-dwm/dwm-date.sh", 1,	0},
+	{"", "~/batinfo.sh", 5,	0},
+	{"", "~/syscfg/scripts/legacy-xorg-dwm/dwm-date.sh", 1,	0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
