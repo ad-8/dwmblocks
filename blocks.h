de54f40d88900ed513862c0bf532a010fe413289
arthur <arthurfleck8@proton.me>
2026-02-25 14:10:45 +0100

    update all script paths

--- blocks.h
+++ blocks.h
@@ -1,9 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "~/backlight.sh", 30,	0},
-	{"", "~/batinfo.sh", 5,	0},
-	{"", "~/syscfg/scripts/legacy-xorg-dwm/dwm-date.sh", 1,	0},
+	{"", "~/syscfg/scripts/freebsd/backlight.sh", 30,	0},
+	{"", "~/syscfg/scripts/freebsd/vpn.sh", 5,	0},
+	{"", "~/syscfg/scripts/freebsd/battery.sh", 5,	0},
+	{"", "~/syscfg/scripts/freebsd/datetime.sh", 1,	0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
