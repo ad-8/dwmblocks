957c5a1042d4102086b23ece5f2c0a6855d32edd
arthur <arthurfleck8@proton.me>
2026-05-19 07:10:32 +0200

    current config on openbsd

--- blocks.h
+++ blocks.h
@@ -1,10 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "~/syscfg/scripts/freebsd/backlight.sh", 30,	0},
-	{"", "~/syscfg/scripts/freebsd/vpn.sh", 5,	0},
-	{"", "~/syscfg/scripts/freebsd/battery.sh", 5,	0},
-	{"", "~/syscfg/scripts/freebsd/datetime.sh", 1,	0},
+	{"", "joker ~/syscfg/scripts/bb/weather.joke dwm", 600,	0},
+	{"", "~/syscfg/scripts/openbsd/backlight.sh",       30,	0},
+	{"", "~/syscfg/scripts/openbsd/volume.sh",           5,	0},
+	{"", "~/syscfg/scripts/openbsd/vpn.sh",             30,	0},
+	{"", "~/syscfg/scripts/openbsd/battery.sh",         20,	0},
+	{"", "~/syscfg/scripts/freebsd/datetime.sh",         1,	0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
